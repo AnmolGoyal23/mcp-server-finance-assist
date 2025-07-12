20203bc33a8ff6610f30d4c9e6ba8a550fdaf6ab
AnmolGoyal23 <52609786+AnmolGoyal23@users.noreply.github.com>
2025-07-12 15:27:42 +0530

    Initial commit

---
+++ src/main/java/com/ai/financialassistant/FinancialassistantApplication.java
@@ -0,0 +1,13 @@
+package com.ai.financialassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialassistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/financialassistant/FinancialassistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.financialassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialassistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
